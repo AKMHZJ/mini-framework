88c75fd6217631e09d3b2d6adab79577c13e9307
Hasnae 🍒 <125884813+Hasnaaaae@users.noreply.github.com>
2025-06-22 02:34:06 +0000

    button && inputComponent

--- mini-framework/src/framework/event.js
+++ mini-framework/src/framework/event.js
@@ -1,1 +1,24 @@
-// Custom event handling system
+// Custom event handling systemconst button = {
+const button = {
+  tag: "button",
+  attrs: {
+    className: "button",
+    onClick: () => alert("Button clicked!"),
+  },
+  children: ["Click"],
+};
+
+
+const inputComponent = {
+  tag: "input",
+  attrs: {
+    type: "text",
+    placeholder: "Typing...😊",
+    onInput: (e) => console.log("Typing:", e.target.value),
+    onKeyPress: (e) => {
+      if (e.key === "Enter") {
+        console.log("Enter pressed!");
+      }
+    },
+  },
+};
